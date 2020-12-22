a44d7c21662ee4ad8d2ecb0ea3498ad34fb9299a
Tomás García Gobet <tomasgobet@icloud.com>
2020-12-22 11:53:16 -0500

    add axios, api context

---
+++ src/contexts/api.tsx
@@ -0,0 +1,27 @@
+import React, { createContext, ReactNode, useContext } from 'react';
+import axios, { AxiosPromise } from 'axios';
+
+interface ApiContext {
+  fetchAlbums: (limit: number) => AxiosPromise;
+}
+
+const initialState: ApiContext = {
+  fetchAlbums: () => new Promise(() => {}),
+};
+
+const ApiContext = createContext(initialState);
+
+export const ApiProvider = ({ children }: { children: ReactNode }) => {
+  
+  const fetchAlbums = (limit = 100) => {
+    return axios.get(`https://itunes.apple.com/ar/rss/topalbums/limit=${limit}/json`);
+  }
+
+  return (
+    <ApiContext.Provider value={{ fetchAlbums }}>
+      {children}
+    </ApiContext.Provider>
+  );
+};
+
+export const useApi = () => useContext(ApiContext);
