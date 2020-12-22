92c3eb7e20893e62e266a1fbc2b29f23327db04d
Tomás García Gobet <tomasgobet@icloud.com>
2020-12-22 10:54:30 -0500

    update: eslint.js config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
+    'prettier/@typescript-eslint'
   ],
   settings: {
     react: {
@@ -30,6 +29,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     "no-restricted-imports": [
       "error",
       {
